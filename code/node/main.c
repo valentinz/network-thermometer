af5372bc5c6e32caf0b5957a4e17a514e7fd954c
Valentin Zickner <zickner@querformatik.de>
2014-10-12 12:42:07 +0200

    Added voltage control for node.

--- code/node/main.c
+++ code/node/main.c
@@ -9,10 +9,24 @@
 unsigned char command = 0x00;
 
 
+float vcc;//variable to hold the value of Vcc
+
+void read_vcc(void)
+{
+	ADMUX = 0xE; //Set the Band Gap voltage as the ADC input
+	ADCSRA = (1<<ADEN)|(1<<ADIE)|(1<<ADSC)|5;
+}
+
+ISR(ADC_vect) //ADC End of Conversion interrupt 
+{
+	unsigned char adc_data;
+	adc_data = ADC>>2; //read 8 bit value
+	vcc = 1.2 * 255 / adc_data * 10;
+}
+
 int main (void) {
 	DDRC = 0xFF;
 	PORTC = 0x00;
-//	uart_init();
 	DDRD = 0xFF;
 	PORTD = 0xFF;
 
@@ -22,31 +36,25 @@ int main (void) {
 		0x01,0x31,0x32,0x33,0x34,0x35,0x36,0x37,0x38,
 		0x39,0x3a,0x3b,0x3c,0x3d,0x3e,0x3f,0x78};
 
+	sei(); //Activate interrupts
+
 	rfm70_init();
 	_delay_ms(2000);
 
-	//rfm70_mode_transmit();
+	rfm70_mode_transmit();
 
 	i = 0;
 
 	while(1) {
-		// receiver
-		_delay_ms(20);
-		if( rfm70_receive( &pipe, &rx_buf, &len )){
-			PORTD = rx_buf[0];
-			rfm70_register_write( RFM70_CMD_FLUSH_RX,0 );
-		}
-
-		/*
+		read_vcc(); //setup the ADC
 		// transmitter
 		_delay_ms(500);
 		tx_buf[0] = i++;
-		rfm70_transmit_message_once( tx_buf, 17 );
+		tx_buf[1] = vcc;
+		rfm70_transmit_message_once( tx_buf, 2 );
 		PORTD = 0x00;
 		_delay_ms(500);
 		PORTD = 0xFF;
-		*/
-
 	}
    
 	return 0;
